e0471ef9a4572d32eccd936cd330682dad87e855
gitnayan <107099130+testgithubnayan@users.noreply.github.com>
2022-08-25 16:28:07 +0530

    basics

---
+++ QuotientReminder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.basics;
+
+public class QuotientReminder {
+    public static void main(String[] args) {
+        int divinder = 45;
+        int divisor = 5;
+
+        int quotient = divinder / divisor;
+        int reminder = divinder % divisor;
+
+        System.out.println("Quotient is: "+quotient);
+        System.out.println("Reminder is: "+reminder);
+    }
+}
